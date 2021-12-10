aa136b7ad177fc751c9e515f0ea411c0d6ea077a
Nir9000 <NirZaid6@gmail.com>
2021-12-11 01:14:28 +0200

    Key Animations

---
+++ Assets/Scripts/Doors and Keys/Door.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private BoxCollider2D collider;
+    private Animator anim;
+    private int numberOfKeysLeft = 0;
+
+    private void Awake()
+    {
+        collider = GetComponent<BoxCollider2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        var keys = FindObjectsOfType<Key>();
+        foreach (var key in keys)
+        {
+            key.OnKeyUsed += UpdateKeyCount;
+            numberOfKeysLeft++;
+        }
+    }
+    
+    private void Open()
+    {
+        anim.SetBool("isOpen", true);
+    }
+
+    public void DisableCollider() // called by animation event
+    {
+        collider.enabled = false;
+    }
+
+    void UpdateKeyCount()
+    {
+        numberOfKeysLeft--;
+        if (numberOfKeysLeft == 0)
+        {
+            Open();
+        }
+    }
+
+
+}
