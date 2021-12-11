43d9bba4cd07fe4fa6c0d3cb289ed73077631b9d
Nir9000 <NirZaid6@gmail.com>
2021-12-11 18:41:22 +0200

    Hourglass finished

--- Assets/Scripts/PlayerControl/GravityControl.cs
+++ Assets/Scripts/PlayerControl/GravityControl.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Audio;
 using Gravity;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace PlayerControl
 {
