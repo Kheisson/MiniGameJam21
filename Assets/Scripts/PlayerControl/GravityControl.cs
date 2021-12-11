343bf1867027e5931d09e88d1e13e064c93bfabe
Kson <igor.kheison@gmail.com>
2021-12-11 10:21:13 +0200

    Implemented gravity change

---
+++ Assets/Scripts/PlayerControl/GravityControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace PlayerControl
+{
+    public static class GravityControl
+    {
+        private static List<Rigidbody2D> _rigids = new List<Rigidbody2D>();
+        private static bool _top;
+        
+        private static void CollectAllBodies()
+        {
+            _rigids = Object.FindObjectsOfType<Rigidbody2D>().ToList();
+            ChangeGravity();
+        }
+
+        private static void ChangeGravity()
+        {
+            foreach (var rb in _rigids)
+            {
+                rb.gravityScale *= -1;
+                if (_top == false)
+                    rb.gameObject.transform.eulerAngles = new Vector3(0, 0, 180);
+                else
+                    rb.gameObject.transform.eulerAngles = Vector3.zero;
+            }
+
+            _top = !_top;
+        }
+
+        public static void Switch()
+        {
+            CollectAllBodies();
+        }
+    }
+}
--- Assets/Scripts/PlayerControl/PlayerInput.cs
+++ Assets/Scripts/PlayerControl/PlayerInput.cs
@@ -1,5 +1,6 @@
 using Animations;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace PlayerControl
 {
@@ -8,18 +9,25 @@ namespace PlayerControl
         private const string HORIZONAL_INPUT_NAME = "Horizontal";
         private const KeyCode JUMP_INPUT_NAME = KeyCode.Space;
 
+        [Header("Component list")][Space(5)]
         [SerializeField] private Rigidbody2D rb;
         [SerializeField] private Animator playerAnimator;
-        [SerializeField] private SpriteRenderer playerSprite;
+        [SerializeField] private Transform playersBody;
         [SerializeField] private BoxCollider2D playerCollider;
-        
-        [Header("Jump collider layer")] [SerializeField] private LayerMask layer;
-        [Header("Ground Check Distance")] [SerializeField] private float groundCheckDistance = 0.5f;
-        [Header("Speed")] [SerializeField] private float playerSpeed = 1f;
+
+        [Header("Jump settings")] 
+        [SerializeField] private LayerMask collideLayerMask;
+        [SerializeField] private Transform playersFeet;
+        [SerializeField] private float collideRadius;
+
+        [Header("Configures")] 
+        [SerializeField] private float playerSpeed = 3f;
+        [SerializeField] private float jumpForce = 500f;
+        [SerializeField] private float moveFeel = 0.5f;
 
         private PlayerAnimation _pa;
-        private float _jumpForce = 500f;
-        private float _moveFeel = 0.5f;
+        private bool _facingRight = true;
+        
 
         private void Start()
         {
@@ -33,47 +41,55 @@ namespace PlayerControl
 
         private void Update()
         {
+            //Handling jumping
             if (Input.GetKeyDown(JUMP_INPUT_NAME) && IsGrounded)
                 Jump();
             _pa.HandleFalling(IsGrounded, rb.velocity.y);
+            
+            //Handling gravitation
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                GravityControl.Switch();
+                _facingRight = !_facingRight;
+            }
         }
 
         private void Move(float input)
         {
-            rb.velocity += Vector2.right * input * playerSpeed;
-            if (rb.velocity.x > _moveFeel || rb.velocity.x < -_moveFeel)
-            {
-                _pa.Run(true);
-            }
-            else
-            {
-                _pa.Run(false);
-            }
+            rb.velocity = new Vector2(input * playerSpeed, rb.velocity.y);
 
-            if (input < 0f)
-            {
-                playerSprite.flipX = true;
-            }
-            else if (input > 0.1f)
-            {
-                playerSprite.flipX = false;
-            }
+            //Handle running animation
+            if (rb.velocity.x > moveFeel || rb.velocity.x < -moveFeel)
+                _pa.Run(true);
+            else
+                _pa.Run(false);
+
+            //Handle Flipping of character
+            if (_facingRight == false && rb.velocity.x > 0)
+                FlipDirection();
+            else if (_facingRight == true && rb.velocity.x < 0)
+                FlipDirection();
+        }
+
+        private void FlipDirection()
+        {
+            _facingRight = !_facingRight;
+            var newScale = playersBody.localScale;
+            newScale.x *= -1;
+            playersBody.localScale = newScale;
         }
 
         private void Jump()
         {
-            rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Force);
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Force);
             _pa.HandleJump();
         }
 
         private bool GroundCheck()
         {
-            RaycastHit2D hit = Physics2D.BoxCast(playerCollider.bounds.center, playerCollider.bounds.size, 0f,
-                Vector2.down, groundCheckDistance,layer);
-            return hit.collider != null;
+            return Physics2D.OverlapCircle(playersFeet.position, collideRadius, collideLayerMask);;
         }
 
         private bool IsGrounded => GroundCheck();
     }
-
 }
