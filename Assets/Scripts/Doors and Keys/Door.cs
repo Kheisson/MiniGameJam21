7f26dc158a03afc309368bdf4ba821a74a1ddd09
Nir9000 <NirZaid6@gmail.com>
2021-12-11 13:28:55 +0200

    Created 2 levels

--- Assets/Scripts/Doors and Keys/Door.cs
+++ Assets/Scripts/Doors and Keys/Door.cs
@@ -3,46 +3,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Door : MonoBehaviour
+namespace DoorsAndKeys
 {
-    private BoxCollider2D collider;
-    private Animator anim;
-    private int numberOfKeysLeft = 0;
-
-    private void Awake()
+    public class Door : MonoBehaviour
     {
-        collider = GetComponent<BoxCollider2D>();
-        anim = GetComponent<Animator>();
-    }
+        private BoxCollider2D collider;
+        private Animator anim;
+        private int numberOfKeysLeft = 0;
 
-    private void OnEnable()
-    {
-        var keys = FindObjectsOfType<Key>();
-        foreach (var key in keys)
+        private void Awake()
         {
-            key.OnKeyUsed += UpdateKeyCount;
-            numberOfKeysLeft++;
+            collider = GetComponent<BoxCollider2D>();
+            anim = GetComponent<Animator>();
         }
-    }
-    
-    private void Open()
-    {
-        anim.SetBool("isOpen", true);
-    }
 
-    public void DisableCollider() // called by animation event
-    {
-        collider.enabled = false;
-    }
-
-    void UpdateKeyCount()
-    {
-        numberOfKeysLeft--;
-        if (numberOfKeysLeft == 0)
+        private void OnEnable()
         {
-            Open();
+            var keys = FindObjectsOfType<Key>();
+            foreach (var key in keys)
+            {
+                key.OnKeyUsed += UpdateKeyCount;
+                numberOfKeysLeft++;
+            }
         }
+
+        private void Open()
+        {
+            anim.SetBool("isOpen", true);
+        }
+
+        public void DisableCollider() // called by animation event
+        {
+            collider.enabled = false;
+        }
+
+        void UpdateKeyCount()
+        {
+            numberOfKeysLeft--;
+            if (numberOfKeysLeft == 0)
+            {
+                Open();
+            }
+        }
+
+
     }
-
-
 }
--- Assets/Scripts/LevelLoading/LevelExit.cs
+++ Assets/Scripts/LevelLoading/LevelExit.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+
 public class LevelExit : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
