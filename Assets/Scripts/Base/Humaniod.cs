f5ee866bf44d059fd4f074caf400469183c9de82
Kson <igor.kheison@gmail.com>
2021-12-11 12:44:01 +0200

    Implement Enemy

---
+++ Assets/Scripts/Base/Humaniod.cs
@@ -0,0 +1,33 @@
+using Gravity;
+using UnityEngine;
+
+namespace Base
+{
+    public abstract class Humaniod : MonoBehaviour, IImpactable
+    {
+        private bool _top;
+        protected bool _facingRight = true;
+        protected Rigidbody2D rb;
+        protected Transform body;
+
+        public virtual void Flip()
+        {
+            rb.gravityScale *= -1;
+            if (_top == false)
+                rb.gameObject.transform.eulerAngles = new Vector3(0, 0, 180);
+            else
+                rb.gameObject.transform.eulerAngles = Vector3.zero;
+
+            _top = !_top;
+        }
+    
+        protected void FlipDirection()
+        {
+            _facingRight = !_facingRight;
+            var newScale = body.localScale;
+            newScale.x *= -1;
+            body.localScale = newScale;
+        }
+        
+    }
+}
